333bade06e57e2b2fc361f1d2ee896f570289571
Aidan Clark <clar2653@d.umn.edu>
2026-02-28 15:10:09 -0600

    back to creative scene, kinda forgot my light positions

--- src/render_main.cpp
+++ src/render_main.cpp
@@ -18,8 +18,8 @@ int main(int argc, char *argv[]) {
     sivelab::GraphicsArgs args;
     args.process(argc, argv);
 
-     point3 eye = point3(0, 5, -1);
-    vec3 direction = vec3(0, 0, -1);
+    point3 eye = point3(0, 5,  2);
+    vec3 direction = vec3(0, -0.5, -1);
     float focalLength = 1.0;
     float imageplaneWidth = 1.0;
 
@@ -33,9 +33,12 @@ int main(int argc, char *argv[]) {
 
     //lights
 
+    //if you were trying to recreate my scene i forgot the light pos and colors i used while making my other scenes
     std::shared_ptr<Light> l = std::make_shared<Light>(point3(3, 12, -10), vec3(1,1,1));
+    std::shared_ptr<Light> lTwo = std::make_shared<Light>(point3(3, 14, -10), vec3(0.32,0.63,0.02));
     
     scene->pushLight(l);
+    scene->pushLight(lTwo);
     
 
     // shaders
@@ -44,8 +47,28 @@ int main(int argc, char *argv[]) {
     std::shared_ptr<Mirror> mi = std::make_shared<Mirror>();
 
     // ground sphere 
+    scene->pushShape(std::make_shared<Sphere>(point3(0, -1000, -10), 995.0f, vec3(0.184, 0.929, 0.294), lam));
     
-    scene->pushShape(std::make_shared<Sphere>(point3(0, 4, -25), 5, vec3(0.84, 0.21, 0.04), lam));
+
+    scene->pushShape(std::make_shared<Triangle>(point3(3,-1,-6),   point3(0,3,-6), point3(2.12,-1,-4.59), vec3(1.0, 0.2, 0.2), lam));
+    scene->pushShape(std::make_shared<Triangle>(point3(2.12,-1,-4.59), point3(0,3,-6), point3(0,-1,-4),   vec3(1.0, 0.6, 0.0), lam));
+    scene->pushShape(std::make_shared<Triangle>(point3(0,-1,-4),   point3(0,3,-6), point3(-2.12,-1,-4.59),vec3(1.0, 1.0, 0.0), lam));
+    scene->pushShape(std::make_shared<Triangle>(point3(-2.12,-1,-4.59), point3(0,3,-6), point3(-3,-1,-6), vec3(0.2, 0.8, 0.2), lam));
+    scene->pushShape(std::make_shared<Triangle>(point3(-3,-1,-6),  point3(0,3,-6), point3(-2.12,-1,-7.41),vec3(0.0, 0.6, 1.0), lam));
+    scene->pushShape(std::make_shared<Triangle>(point3(-2.12,-1,-7.41), point3(0,3,-6), point3(0,-1,-8),  vec3(0.4, 0.0, 1.0), lam));
+    scene->pushShape(std::make_shared<Triangle>(point3(0,-1,-8),   point3(0,3,-6), point3(2.12,-1,-7.41), vec3(0.8, 0.0, 0.8), lam));
+    scene->pushShape(std::make_shared<Triangle>(point3(2.12,-1,-7.41), point3(0,3,-6), point3(3,-1,-6),   vec3(1.0, 0.2, 0.5), lam));
+
+    scene->pushShape(std::make_shared<Sphere>(vec3(4,-3,-6), 1.0f, vec3(1,1,0), lam));
+    scene->pushShape(std::make_shared<Sphere>(vec3(-4,-3,-6), 1.0f, vec3(0,1,1), bf));
+    scene->pushShape(std::make_shared<Sphere>(vec3(0,-3,-5), 1.0f, vec3(1,0,0), lam));
+
+    scene->pushShape(std::make_shared<Sphere>(vec3(-3, 2,-8), 2.0f, vec3(0,1,0), mi));
+    scene->pushShape(std::make_shared<Sphere>(vec3(3, 2,-8), 2.0f, vec3(0,1,0), mi));
+   
+    scene->pushShape(std::make_shared<Sphere>(vec3(15, 0,4), 6.0f, vec3(1,1,1), lam));
+    scene->pushShape(std::make_shared<Sphere>(vec3(15, 6, 4), 4.0f, vec3(1,1,1), lam));
+    scene->pushShape(std::make_shared<Sphere>(vec3(16, 10, 4), 3.0f, vec3(1,1,1), lam));
     
     
   
@@ -54,5 +77,5 @@ int main(int argc, char *argv[]) {
     r.render(scene, fb, persCam);
 
     //export to png
-    fb.writeToPNG("withantialiasing.png");
+    fb.writeToPNG("testtest.png");
 }
